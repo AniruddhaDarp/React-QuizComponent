34a65e8d7d6d0cecab73564f83302109c46ee465
anirdarp <anirdarp@amazon.com>
2020-03-04 17:13:43 -0500

    Handling negative events

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { incorrectAnswer: false }
+  }
   handleClick(buttonText) {
     if(buttonText === this.props.quiz_question.answer) {
+      this.setState((state) => {
+        return { incorrectAnswer: false }
+      })
       this.props.showNextQuestionHandler()
+    } else {
+      this.setState((state) => {
+        return { incorrectAnswer: true }
+      })
     }
   }
   render() {
     return (
       <main>
         <section>
-          <p>{this.props.quiz_question.instruction_text}</p>
+          {this.state.incorrectAnswer ? (<p className='error'>Sorry, that's not right</p>) : null}
         </section>
         <section className="buttons">
           <ul>
