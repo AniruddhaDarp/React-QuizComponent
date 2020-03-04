1fa8ab899951625bf18d9a86439be575ff2f6ce2
anirdarp <anirdarp@amazon.com>
2020-03-04 10:53:35 -0500

    Adding div

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,15 +5,14 @@ let quizData = require('./quiz_data.json')
 class Quiz extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {quiz_position: 1}
+    this.state = { quiz_position: 1 }
   }
-
   render() {
-    let questions = quizData.quiz_questions
-    let first = questions[0].instruction_text
     return (
-      <div class='QuizQuestion'>
-        {first}
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
       </div>
     )
   }
